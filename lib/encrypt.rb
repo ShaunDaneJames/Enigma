110c6b892d84250eaa7ea48f42211a43da3e4513
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-20 16:19:57 -0600

    Fixes shiftable module

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,11 +1,12 @@
 require 'date'
 require_relative 'message_data'
+require_relative 'shiftable'
 
 class Encrypt < MessageData
+include Shiftable
   attr_reader :alphabet
 
   def initialize
-    #include shiftable
     @alphabet = ("a".."z").to_a << " "
     @random_number = []
     @a = nil
@@ -76,31 +77,7 @@ class Encrypt < MessageData
         @encrypted_message
       end
     end
-    @encrypted_message
+    @encrypted_message.join
   end
 
-
-  #>>>>>>>>>>>shiftable?
-  def random_5_digit_number
-    5.times do
-      @random_number << rand(9)
-    end
-  end
-
-  def generate_shifts
-    @a = @random_number[0..1].join.to_i + offset[0]
-    @b = @random_number[1..2].join.to_i + offset[1]
-    @c = @random_number[2..3].join.to_i + offset[2]
-    @d = @random_number[3..4].join.to_i + offset[3]
-  end
-
-  def offset
-    sq_date_time = Time.now.strftime("%m%d%y").to_i
-    offsets = sq_date_time * sq_date_time
-    last_four = offsets.to_s.split("").last(4)
-    last_four.map do |number|
-      number.to_i
-    end
-  end
-  #<<<<<<<<<<<<shiftable?
 end
