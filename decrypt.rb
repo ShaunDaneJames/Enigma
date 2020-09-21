7b79bb6da658df5030abe74122eda7c8e3c17c9d
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-21 13:33:49 -0600

    Sets up runners and outputs name of save file, key, and date

---
+++ decrypt.rb
@@ -0,0 +1,14 @@
+require './lib/enigma'
+require './lib/decode'
+
+message = File.open(ARGV[0], "r")
+incoming = message.read.chomp
+enigma = Enigma.new
+decrypting_message = enigma.decrypt(incoming, "02715", "040895")
+outgoing = File.open(ARGV[1], "w")
+
+outgoing.write(decrypting_message[:message])
+
+outgoing.close
+
+puts "Created #{ARGV[1]} with the key #{enigma.decrypted[:key]} and date #{enigma.decrypted[:date]}"
